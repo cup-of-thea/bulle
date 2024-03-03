73ac1533c42b975bfea7e1107584e0139ceb9b8d
Thea <thea.joy@outlook.com>
2024-03-03 04:15:07 +0100

    feat(last-posts): introduce last posts query

---
+++ app/Domain/UseCases/LastPostsQuery.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Domain\UseCases;
+
+use App\Domain\ValueObjects\PostItem;
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+use Thea\MarkdownBlog\Domain\ValueObjects\Category;
+
+class LastPostsQuery
+{
+    public function get(): Collection
+    {
+        return DB::table('posts as p')
+            ->select('p.id', 'p.title', 'p.slug', 'p.date', 'p.content', 'c.title as category_title', 'c.slug as category_slug',)
+            ->leftJoin('categories as c', 'p.category_id', '=', 'c.id')
+            ->orderBy('date', 'desc')
+            ->get()
+            ->map(fn($post) => PostItem::from(
+                $post->title,
+                $post->slug,
+                $post->content,
+                new Carbon($post->date),
+                $post->category_slug ? Category::from($post->category_title, $post->category_slug) : null,
+                DB::table('post_author as pa')
+                    ->select('a.name', 'a.slug')
+                    ->join('authors as a', 'pa.author_id', '=', 'a.id')
+                    ->where('pa.post_id', $post->id)
+                    ->get()
+                    ->toArray(),
+            ))->collect();
+    }
+}
