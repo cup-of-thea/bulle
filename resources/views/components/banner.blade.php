b4089a9145f2871509ab4328f67027baeeec7970
Magali <51261454+TheDaisyMoon@users.noreply.github.com>
2024-03-04 10:00:22 +0100

    Corrections magazine
    Corrections mot magazine

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -16,7 +16,7 @@
             <svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
                 <circle cx="1" cy="1" r="1"/>
             </svg>
-            Nous sommes heureuxses de vous annoncer que la première édition de notre magasine est enfin disponible&nbsp;!
+            Nous sommes heureuxses de vous annoncer que la première édition de notre magazine est enfin disponible&nbsp;!
         </p>
     </div>
     <div class="flex flex-1 justify-end">
--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -36,7 +36,7 @@
                             <div class="mt-10 flex items-center gap-x-6">
                                 <a href="{{ route('blog') }}"
                                    class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                    Le Magasine
+                                    Le Magazine
                                 </a>
                                 <a href="{{ route('association') }}"
                                    class="text-sm font-semibold leading-6 text-gray-900">L'Association <span
