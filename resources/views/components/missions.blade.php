7e0b32e21b51b8b417e4bf6d32d0bc3c11b0ac24
Magali <51261454+TheDaisyMoon@users.noreply.github.com>
2024-03-04 18:43:30 +0100

    chore: change pitch hero and association

--- resources/views/components/missions.blade.php
+++ resources/views/components/missions.blade.php
@@ -1,31 +1,29 @@
 <div class="bg-white py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
-            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our mission</h2>
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Notre combat</h2>
             <div class="mt-6 flex flex-col gap-x-8 gap-y-20 lg:flex-row">
                 <div class="lg:w-full lg:max-w-2xl lg:flex-auto">
-                    <p class="text-xl leading-8 text-gray-600">Aliquet nec orci mattis amet quisque ullamcorper neque,
-                        nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas
-                        ac. Diam nulla orci at in viverra scelerisque eget. Eleifend egestas fringilla sapien.</p>
-                    <p class="mt-10 max-w-xl text-base leading-7 text-gray-700">Faucibus commodo massa rhoncus,
-                        volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id
-                        dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.
-                        Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper
-                        sed amet vitae sed turpis id.</p>
+                    <p class="text-xl leading-8 text-gray-600">Femmes et personnes non-binaires de la Tech, nous évoluons dans une industrie qui nous confronte quotidiennement à du sexisme plus ou moins ordinaire et à des enjeux d’oppressions divers que nous ne pouvons pas ignorer. Ensemble, nous constatons qu’il est très difficile de faire entendre nos voix et de parler de ces sujets. Seules, parler du sexisme est risqué, c’est se rendre vulnérables, risquer d’être attaquées, et nous sommes fatiguées de devoir porter cette lutte chacune de notre côté.
+                    </p>
+                    <p class="mt-10 max-w-xl text-base leading-7 text-gray-700">Nous avons donc décidé de la porter ensemble. Et pour cela, quoi de mieux qu’un magazine féministe ? La Première Ligne est un espace où les femmes et personnes non-binaires de la tech peuvent s’exprimer sur des sujets ayant un lien avec le sexisme, le racisme, les lgbtqia+phobies, le validisme, la grossophobie, l’antisémitisme, la psychophobie, et tous les axes d’oppression en général, afin de les déconstruire et les combattre. Nous parlons de ces sujets à travers tout le prisme de nos existences, du plus banal au plus important, du plus ludique au plus sérieux et cherchons toujours à amener un contenu global visant l’amélioration de l’inclusion dans notre société en général, et la tech en particulier.</p>
+
+                    <p class="mt-10 max-w-xl text-xl leading-7 text-gray-800 font-bold">
+                        Nous sommes la première ligne, celle qui commence le combat, qui trace le chemin pour les autres, et qui débute le récit.</p>
                 </div>
                 <div class="lg:flex lg:flex-auto lg:justify-center">
                     <dl class="w-64 space-y-8 xl:w-80">
                         <div class="flex flex-col-reverse gap-y-4">
-                            <dt class="text-base leading-7 text-gray-600">Transactions every 24 hours</dt>
-                            <dd class="text-5xl font-semibold tracking-tight text-gray-900">44 million</dd>
+                            <dt class="text-base leading-7 text-gray-600">supers autrices débordées mais motivées</dt>
+                            <dd class="text-5xl font-semibold tracking-tight text-gray-900">10</dd>
                         </div>
                         <div class="flex flex-col-reverse gap-y-4">
-                            <dt class="text-base leading-7 text-gray-600">Assets under holding</dt>
-                            <dd class="text-5xl font-semibold tracking-tight text-gray-900">$119 trillion</dd>
+                            <dt class="text-base leading-7 text-gray-600">édition en 2024 (et d'autres arrivent !)</dt>
+                            <dd class="text-5xl font-semibold tracking-tight text-gray-900">1</dd>
                         </div>
                         <div class="flex flex-col-reverse gap-y-4">
-                            <dt class="text-base leading-7 text-gray-600">New users annually</dt>
-                            <dd class="text-5xl font-semibold tracking-tight text-gray-900">46,000</dd>
+                            <dt class="text-base leading-7 text-gray-600">idées d'articles (à peu près)</dt>
+                            <dd class="text-5xl font-semibold tracking-tight text-gray-900">785 145 551</dd>
                         </div>
                     </dl>
                 </div>
