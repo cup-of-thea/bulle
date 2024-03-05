0cc55488b72df3b5c52f4c489a433db4b2787328
Thea <thea.joy@outlook.com>
2024-03-05 02:25:16 +0100

    refactor(posts): introduce posts repository
    Signed-off-by: Thea <thea.joy@outlook.com>

--- app/Adapters/InfrastructureServiceProvider.php
+++ app/Adapters/InfrastructureServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Adapters;
 
 use App\Adapters\Repositories\AuthorsRepository;
 use App\Adapters\Repositories\CategoriesRepository;
+use App\Adapters\Repositories\PostsRepository;
 use App\Domain\Repositories\IAuthorsRepository;
 use App\Domain\Repositories\ICategoriesRepository;
+use App\Domain\Repositories\IPostsRepository;
 use Illuminate\Support\ServiceProvider;
 
 class InfrastructureServiceProvider extends ServiceProvider
@@ -13,5 +15,6 @@ class InfrastructureServiceProvider extends ServiceProvider
     public array $bindings = [
         IAuthorsRepository::class => AuthorsRepository::class,
         ICategoriesRepository::class => CategoriesRepository::class,
+        IPostsRepository::class => PostsRepository::class,
     ];
 }
---
+++ app/Domain/Repositories/IPostsRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Domain\Repositories;
+
+use App\Domain\ValueObjects\Category;
+use Illuminate\Support\Collection;
+
+interface IPostsRepository
+{
+
+    public function getPostsFromCategory(Category $category): Collection;
+}
