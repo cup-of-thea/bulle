143df55de783cb371bd05ad238c811c1f88e430b
Magali <51261454+TheDaisyMoon@users.noreply.github.com>
2024-06-19 10:07:02 +0200

    chore-second-edition

--- config/authors.php
+++ config/authors.php
@@ -18,7 +18,7 @@ return [
         ],
         'title' => 'Créatrice de sites et inclusion advocate',
         'image' => '/img/authors/magali.jpg',
-        'bio' => "Ancienne travailleuse sociale devenue dev, je suis féministe depuis le biberon et je travaille sur les sujets d'éthique, d'inclusion et de comment changer la société pour détruire les oppressions depuis plus de vingt ans. J'essaie de faire bouger les lignes à autant de niveaux possibles. 🌼🌛 Co-fondatrice et présidente de Compositech.",
+        'bio' => "Ancienne travailleuse sociale devenue dev, je suis féministe depuis le biberon et je travaille sur les sujets d'éthique, d'inclusion et de comment changer la société pour détruire les oppressions depuis plus de vingt ans. J'essaie de faire bouger les lignes à autant de niveaux possibles. 🌼🌛 Co-fondatrice et présidente de Compositech. Fondatrice et présidente de La Première Ligne.",
     ],
     'thea' => [
         'links' => [
@@ -33,7 +33,7 @@ return [
         'title' => 'Senior Software Engineer',
         'image' => '/img/authors/thea.jpg',
         'bio' => "Développeuse · Affiliée Twitch · Autrice de contenus · Féministe Intersectionnelle & Lesbienne et fière de l'être 🌸
-                  Je poste sur mes lectures, l’inclusion et ma transidentité sur mon blog ☕️",
+        Je poste sur mes lectures, l’inclusion et ma transidentité sur mon blog ☕️",
     ],
     'emmanuelle-aboaf' => [
         'links' => [
@@ -47,5 +47,23 @@ return [
         'title' => 'Dev web back',
         'bio' => " Côté pro, je fais du PHP et du SQL. Côté perso, j'aime lire, la science-fiction, la linguistique, les chats, l'informatique, les jeux vidéo, le féminisme, les théories queer, entre autre.",
         'image' => '/img/authors/solene.jpg',
+    ],
+    'les-invitees' => [
+        'bio' => "L'équipe de La Première Ligne invite régulièrement des personnes extérieures à exprimer leurs voix dans notre magazine. C'est sous cette bannière que vous retrouverez tous les articles des invitées. ",
+        'image' => '/img/authors/invitees.png',
+    ],
+    'anonyme' => [
+        'bio' => "Certains sujets sont plus difficiles que d'autres à assumer publiquement. Afin d'assurer la sécurité (émotionnelle mais aussi physique) des autrices, nous publions certains articles en anonyme.",
+        'image' => '/img/authors/anonymes.png',
+    ],
+    'marine-gandy' => [
+        'links' => [
+            'iconoir-twitter' => 'https://x.com/mupsigraphy',
+            'iconoir-linkedin' => 'https://www.linkedin.com/in/marinegandy/',
+            'iconoir-puzzle' => 'https://compositech.org/',
+        ],
+        'title' => 'Developer Relations Engineer',
+        'image' => '/img/authors/marine.jpg',
+        'bio' => "Dev, conférencière, fana de chats. Après bien des années passées à observer, analyser et affiner mes idées, il est temps pour moi de commencer à m'exprimer. Co-fondatrice et trésorière de Compositech.",
     ],
 ];
