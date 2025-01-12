07cfea7d6778d19a773ab8824d389f16ff8b1c6f
Thea <thea.milesi@agicap.com>
2025-01-12 20:35:05 +0100

    fix(responsive): fix x-scroll on menus
    Closes #50

--- resources/views/components/taxonomies-nav.blade.php
+++ resources/views/components/taxonomies-nav.blade.php
@@ -1,4 +1,4 @@
-<div class="flex gap-x-12 mt-6">
+<div class="grid md:grid-cols-4 grid-cols-2 gap-4 mt-10">
     <a href="{{ route('editions.index') }}" class="font-semibold leading-6 text-gray-900">
         Éditions
     </a>
