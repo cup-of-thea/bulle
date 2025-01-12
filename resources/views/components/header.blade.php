850e80d81817169cc1f0ca22a494ff4406d20ebe
Thea <thea.milesi@agicap.com>
2025-01-12 20:35:05 +0100

    fix(responsive): fix x-scroll on menus
    Closes #50

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,12 +1,12 @@
 <header class="bg-white">
-    <nav class="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
+    <nav class="mx-auto flex max-w-7xl sm:items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
         <div class="flex lg:flex-1">
             <a href="{{ route('home') }}" class="-m-1.5 p-1.5">
                 <span class="sr-only">La Première Ligne</span>
                 <img class="h-8 w-auto" src="/img/icons/logo.png" alt="">
             </a>
         </div>
-        <div class="flex gap-x-12">
+        <div class="grid gap-8 sm:grid-cols-3">
             <a href="{{ route('blog') }}" class="text-sm font-semibold leading-6 text-gray-900">
                 Le Magazine
             </a>
